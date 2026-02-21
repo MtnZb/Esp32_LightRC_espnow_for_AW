3d7fce0aa4a2be0497344cb7d81ceadfc0a40d82
MtnZ <mutant.bol@gmail.com>
2026-02-21 18:13:43 +0300

    Added packed to structure

--- ESP_flashlight/src/main.cpp
+++ ESP_flashlight/src/main.cpp
@@ -4,17 +4,17 @@
 
 const int flashlightPin = LED_BUILTIN;
 typedef struct struct_message {
-    bool Light_On;
-} struct_message;
+  bool Light_On;
+} __attribute__((packed)) struct_message;
 struct_message incomingMessage;
 
 // put function declarations here:
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len);
+void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len);
 
 void setup() {
   Serial.begin(115200);
   pinMode(flashlightPin, OUTPUT);
-  digitalWrite(flashlightPin, HIGH); // Изначально выключаем фонарик (на D1 Mini HIGH выключает встроенный светодиод) не забудьте убрать инверсию логики
+  digitalWrite(flashlightPin, HIGH);  // Изначально выключаем фонарик (на D1 Mini HIGH выключает встроенный светодиод) не забудьте убрать инверсию логики
 
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -23,7 +23,7 @@ void setup() {
     return;
   }
   esp_now_register_recv_cb(OnDataRecv);
-  
+
   Serial.println();
   Serial.print("ESP Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
@@ -34,11 +34,11 @@ void loop() {
 }
 
 // put function definitions here:
-void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
+void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&incomingMessage, incomingData, sizeof(incomingMessage));
-  
+
   if (incomingMessage.Light_On) {
-    digitalWrite(flashlightPin, LOW); // Включаем фонарик (на D1 Mini LOW включает встроенный светодиод) с инверсией логики не забудьте убрать инверсию логики
+    digitalWrite(flashlightPin, LOW);  // Включаем фонарик (на D1 Mini LOW включает встроенный светодиод) с инверсией логики не забудьте убрать инверсию логики
     Serial.println("Light: ON");
   } else {
     digitalWrite(flashlightPin, HIGH);
